4f0912d2d8c6506ceed004d77af1312c88634fa7
jjeda <jjeda@naver.com>
2019-07-13 15:13:16 +0900

    restaruant toy project start

---
+++ src/main/java/me/jjeda/restaruant/RestaruantApplication.java
@@ -0,0 +1,13 @@
+package me.jjeda.restaruant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaruantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaruantApplication.class, args);
+    }
+
+}
